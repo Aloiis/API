74b66b0c7141b575a40b3178423f620b3d2de230
Danilo Santana <dsantana2017@gmail.com>
2022-08-29 11:16:05 -0300

    DELETE via Express

--- index.js
+++ index.js
@@ -2,18 +2,11 @@ const express = require('express')
 
 const app = express()
 
-
 app.listen('3000')
 
-app.use(express.json())
+app.route('/:identificador').delete( (req,res)=> {
+    res.send(req.params.identificador)
 
-let author = "Dan"
-
-app.route('/').get((req,res)=> res.send(author))
-
-app.route('/').put( (req,res)=> {
-    author = req.body
-    res.send(author)
 })
 
 
